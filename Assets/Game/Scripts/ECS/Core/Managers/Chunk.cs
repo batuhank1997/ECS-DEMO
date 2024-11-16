9f48948f7ce5bd65fdc50e248ab13388663de4e5
Batuhan Köroğlu <51181106+batuhank1997@users.noreply.github.com>
2024-11-16 14:00:31 +0300

    update: scripts

---
+++ Assets/Game/Scripts/ECS/Core/Managers/Chunk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Scripts.ECS.Core.Managers
+{
+    public class Chunk
+    {
+        public readonly Archetype Archetype;
+        
+        public readonly List<Entity> Entities;
+        
+        public Chunk(Archetype archetype, int size = 16)
+        {
+            Archetype = archetype;
+            Entities = new List<Entity>();
+        }
+
+        public void AddEntity(Entity entity)
+        {
+            Entities.Add(entity);
+        }
+    }
+}
--- Assets/Game/Scripts/ECS/Core/Managers/ECSManager.cs
+++ Assets/Game/Scripts/ECS/Core/Managers/ECSManager.cs
@@ -31,22 +31,4 @@ namespace Game.Scripts.ECS.Core.Managers
             return newChunk;
         }
     }
-    
-    public class Chunk
-    {
-        public readonly Archetype Archetype;
-        
-        public readonly List<Entity> Entities;
-        
-        public Chunk(Archetype archetype, int size = 16)
-        {
-            Archetype = archetype;
-            Entities = new List<Entity>();
-        }
-
-        public void AddEntity(Entity entity)
-        {
-            Entities.Add(entity);
-        }
-    }
 }
